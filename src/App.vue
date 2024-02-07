b4aa882e426aa686cae7eb0111a07e93dce44bf4
donghyuk <ldh0308@naver.com>
2024-02-07 12:12:24 +0900

    frame setting

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
-  <v-app>
-    <v-main>
-      <Delivery/>
-      <orderinfo/>
-      <Most/>
-      <graphMs/>
-      <Comparison />
+<v-app>
+<v-main>
+<v-container>
+        <v-row no-gutters>
+          <v-col>
+            <v-sheet class="pa-2 ma-2">
+              <Delivery />
+            </v-sheet>
+          </v-col>
+          <v-col>
+            <v-sheet class="pa-2 ma-2">
+              <orderinfo />
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
-    <v-btn>
-  Button
-</v-btn>
   </v-app>
 </template>
 
+<script setup>
+const links = ["Dashboard", "Messages", "Profile", "Updates"];
+</script>
+
 <script>
-import Delivery from './components/Delivery.vue'
-import orderinfo  from './components/orderInfo.vue'
-import Most from './components/Most.vue'
-import graphMs from './components/graph-ms.vue'
-import Comparison from './components/Comparison.vue'
+import Delivery from "./components/Delivery.vue";
+import orderinfo from "./components/orderInfo.vue";
+import Most from "./components/Most.vue";
+import graphMs from "./components/graph-ms.vue";
+import Comparison from "./components/Comparison.vue";
+
 export default {
-  name: 'App',
-components: {
-  Delivery,
-  orderinfo,
-  Most,
-  graphMs,
-  Comparison
-}
-}
+  data: () => ({
+    links: ["Dashboard", "Messages", "Profile", "Updates"],
+    name: "App",
+    components: {
+      Delivery,
+      orderinfo,
+      Most,
+      graphMs,
+      Comparison,
+    },
+  }),
+};
 </script>
