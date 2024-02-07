822a8b06efe516caec30e796b6f1281dd6e380c8
dhkdtld37 <hyeonnls96@naver.com>
2024-02-07 11:42:57 +0900

    0.3.0 lim first commit
    0.3.0 first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
   <v-app>
     <v-main>
-      <Delivery/>
-      <orderinfo/>
-      <Most/>
-      <graphMs/>
-      <Comparison />
+      <v-container>
+        <v-row>
+          <v-col>
+            <Delivery/>
+          </v-col>
+          <v-col>
+            <orderinfo/>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <Most/>
+          </v-col>
+          <v-col>
+            <graphMs/>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <Comparison />
+          </v-col>
+        </v-row>
+        <!-- 추가된 버튼 -->
+        <v-row>
+          <v-col>
+            <v-btn>
+              Button
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
-    <v-btn>
-  Button
-</v-btn>
   </v-app>
 </template>
 
@@ -19,14 +42,15 @@ import orderinfo  from './components/orderInfo.vue'
 import Most from './components/Most.vue'
 import graphMs from './components/graph-ms.vue'
 import Comparison from './components/Comparison.vue'
+
 export default {
   name: 'App',
-components: {
-  Delivery,
-  orderinfo,
-  Most,
-  graphMs,
-  Comparison
-}
+  components: {
+    Delivery,
+    orderinfo,
+    Most,
+    graphMs,
+    Comparison
+  }
 }
 </script>
